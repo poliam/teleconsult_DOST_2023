c0c4e1fceb8ec5c9c2b396ef604faadfdb9acc00
ZaitonZz <philipcandelario1@gmail.com>
2025-07-25 14:16:12 +0800

    fix: remove SetDefaultGroupMiddleware from MIDDLEWARE settings

--- Teleconsult/settings/base.py
+++ Teleconsult/settings/base.py
@@ -40,7 +40,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'Teleconsult.middleware.SetDefaultGroupMiddleware',
 ]
 
 ROOT_URLCONF = 'Teleconsult.urls'
